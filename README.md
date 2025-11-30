604e49e4e7de1ac9de0d0384f7dd6ccb4f89a0ad
Bean07 <ben.martins537@gmail.com>
2025-11-29 19:54:52 -0700

    Work from class work thursday Nov 20.  Activity 1 is complete, measurements are in the README.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 Development branch for lab 10, authored by Brian Stites and Ben Martins
+
+Forward drop of led is 1.723.
+
+Resistor is 217.3 ohm.
+
+Led circiut is running at 3v3, draws 6.65 mA. 
+
+For blinky using sleep ms, the current draw oscillates between 30-34 mA. (off/on)
+
+For blinky using freertos task and vTaskDelay, the current draw oscillates between 34-38 mA. (off/on)
+
+For busy task utilizing ~100% CPU utilization, the current draw is stable around 31 mA. (no LED toggle).
--- src/main.c
+++ src/main.c
@@ -12,8 +12,8 @@
 #include "pico/stdlib.h"
 #include "pico/multicore.h"
 #include "pico/cyw43_arch.h"
+#include <pico/sync.h>
 
-const uint led_pin = 34;
 
 #define MAIN_TASK_PRIORITY      ( tskIDLE_PRIORITY + 1UL )
 #define BLINK_TASK_PRIORITY     ( tskIDLE_PRIORITY + 2UL )
@@ -23,29 +23,56 @@ const uint led_pin = 34;
 
 void ms_blink(__unused void* params)
 {
-    bool on = true;
+    int toggle = 0;
     for(;;)
     {
-        on = !on;
-        cyw43_arch_gpio_put(led_pin, on);
+        toggle = !toggle;
+        gpio_put(LED_PIN, toggle);
         sleep_ms(333);
     }
 }
 
-void runner_task(__unused void* params)
+void blink_task(__unused void* params)
 {
-    TaskHandle_t ms_blink_task;
-    xTaskCreate(ms_blink, "msBlinkTask", MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &ms_blink_task);
-    
+    int toggle = 0;
+    for(;;)
+    {
+        toggle = !toggle;
+        gpio_put(LED_PIN, toggle);
+        vTaskDelay(pdMS_TO_TICKS(333));
+    }
 }
+
+void busy_task(__unused void *params)
+{
+    while(1) {
+        uint32_t k;
+        for (int i = 0; i < 30; i++)
+        {
+            uint32_t j = 0;
+            j = ((~j >> i) + 1) * 27644437;
+            k = j;
+        }
+    }
+}
+
 
 int main( void )
 {
     stdio_init_all();
     hard_assert(cyw43_arch_init() == PICO_OK);
-    cyw43_arch_gpio_put(led_pin, true);
-    TaskHandle_t runner_task_handle;
-    xTaskCreate(runner_task, "RunnerThread", MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &runner_task_handle);
-    vTaskStartScheduler();
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_put(LED_PIN, 0);
+
+    //TaskHandle_t runner_task_handle;
+    //xTaskCreate(busy_task, "RunnerThread", MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &runner_task_handle);
+    //vTaskStartScheduler();
+    
+    busy_task(NULL);
+
+    //ms_blink(NULL);
+    //while(1);
+
     return 0;
 }
